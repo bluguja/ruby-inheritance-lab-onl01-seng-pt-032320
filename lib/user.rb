e8276b6477720c388f155cbb4fdefde8f075f924
Bala Luguja <bluguja@gmail.com>
2020-04-29 05:52:41 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,7 @@ class User
   
   
   
-   #def initialize(first_name,last_name) 
+   #def initialize(first_name,last_name) # not required in the specs
   #   @first_name = first_name
   #   @last_name = last_name
     
