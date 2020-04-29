c7bcfb1522994b335ebf0e018166f8ab6d4bc978
Bala Luguja <bluguja@gmail.com>
2020-04-29 05:42:23 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,10 +3,10 @@ class User
   
   
   
-  # def initialize(first_name,last_name) 
+   def initialize(first_name,last_name) 
   #   @first_name = first_name
   #   @last_name = last_name
-    
+    KNOWLEDGE = []
   # end
   
   
